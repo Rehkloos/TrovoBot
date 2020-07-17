02cc418558bd32bab584a9a70b9c8bb74f01cf08
krammy <keenandavidmoloney@outlook.com>
2020-07-17 22:24:28 +0100

    list command updated

--- plugins/command-list/command-list.js
+++ plugins/command-list/command-list.js
@@ -1,5 +1,4 @@
-const fs = require('fs');
-const path = require('path');
+const Plugins = require('./../../modules/Plugins.js');
 
 module.exports = {
   name: 'Command List',
@@ -7,33 +6,18 @@ module.exports = {
   chat: true, // Defines this as a Chat Command
   event: false, // Is this a Event?
   type: 5004, // Type Event
-  command: 'command-list', // This is the Command that is typed into Chat!
+  command: 'commands', // This is the Command that is typed into Chat!
   permissions: [], // This is for Permissisons depending on the Platform.
   alias: [], // Alias commands that preform interesting things.
   cooldown: 1, // this is Set in Seconds, how long between the next usage of this command.
   settings: false, // Defining this as false will load the Settings file for this Plugin when the system loads this plugin.
-  credits: 'Created by Rehkloos',
+  credits: 'Created by Rehkloos, updated by Krammy',
   execute(client) {
-    const dir = path.join(__dirname, '..');
-
-    try {
-      const files = fs.readdirSync(dir);
-
-      // files object contains all files names
-      // log them on console
-
-      let str = files;
-      const array = str.toString().split(',');
-      // TODO: Need spacing and a "," between each command in the array
-      for (let i = 0; i < array.length; i++) {
-        array[i] = `!${array[i]}`;
-      }
-      str = array.join('');
-
-      // TODO: connect to plugin module so it only list files that are present
-      client.sendMessage(str);
-    } catch (err) {
-      client.sendMessage('There is an error');
-    }
+	  
+	var commands = Array.from( Plugins.chat.keys());  
+	commands = commands.join(" , !");
+	commands = `!${commands}`;
+	client.sendMessage(commands);
+	
   },
 };
